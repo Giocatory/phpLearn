653755176ebc222dec7d1e13353d05178c1ea36d
Giocatory <nivolyas@gmail.com>
2021-11-14 12:31:10 +0300

    14.11.2021

--- index.php
+++ index.php
@@ -5,11 +5,13 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="style/style.css">
     <title>Learn PHP</title>
 </head>
 <body>
-    <?php
-    include "pagesToLearn/create_variables.php"
-    ?>
+<h2>Base</h2>
+<ul>
+    <li><a href="pagesToLearn/create_variables.php" target="_blank">Создание переменных и вывод на экран</a></li>
+</ul>
 </body>
 </html>
