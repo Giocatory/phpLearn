29ea510bc31b9891ac756fee16a1cc53daeddc44
Giocatory <nivolyas@gmail.com>
2021-11-27 20:56:20 +0300

    Work with file system

---
+++ work with file system/example upload file to server/index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Upload file to server</title>
+</head>
+<body>
+<fieldset>
+    <legend>Отправка Файла на сервер</legend>
+    <form action="parse.php" method="post" enctype="multipart/form-data">
+        <input type="hidden" name="MAX_FILE_SIZE" value="30000">
+        <label>Загрузить файл: </label><br>
+        <input type="file" name="myfile"><br>
+        <input type="submit" value="Отпавить фаил">
+    </form>
+</fieldset>
+</body>
+</html>
---
+++ work with file system/example upload file to server/parse.php
@@ -0,0 +1,14 @@
+<?php
+//where save upload files
+$upload_dir = 'C:\localhost\phpLearn\work with file system\example upload file to server\upload';
+$destination = $upload_dir . "\\" . $_FILES['myfile']['name'];
+print "<pre>";
+if (move_uploaded_file($_FILES['myfile']['tmp_name'], $destination)){
+    /*Перемещаем файл из временной папки в выбранную директорию для хранения*/
+    echo "Фаил успешно загружен!";
+}
+else{
+    echo "Произошла какая-то ошибка: <br>";
+    print_r($_FILES);
+}
+echo "</pre>";
