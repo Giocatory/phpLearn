7133ee78f3d3aae22823c499ec4a9130df906877
Giocatory <nivolyas@gmail.com>
2021-12-05 18:46:16 +0300

    lamda and callback functions

---
+++ functions and simple definitions/lambda_and_callback.php
@@ -0,0 +1,22 @@
+<?php
+// lambda function (or anonymous function)
+$hello = function ($user) {echo "Hello, $user!";};
+$hello('Mikhail');  // Hello, Mikhail!
+echo "\n\n";
+
+// callback function
+function greet(callable $anon){
+    $anon('Tatyana');
+}
+greet($hello);  // Hello, Tatyana!
+echo "\n\n";
+
+
+function meet(): callable {
+    $time = 'morning';
+    return function ($name) use (&$time) {
+        return "Good $time, $name!";
+    };
+}
+$meeting = meet();
+echo $meeting('Margo');  // Good morning, Margo!
